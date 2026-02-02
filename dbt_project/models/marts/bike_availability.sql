06d2df96f74f1c7377e1272c879ed0a10b12f4b6
ghhrrrrrr <ghhrrrrrr@gmail.com>
2026-02-02 01:15:36 +0100

    feat: project structure improved

--- dbt_project/models/marts/bike_availability.sql
+++ dbt_project/models/marts/bike_availability.sql
@@ -8,8 +8,10 @@ select
     station_id,
     updated_at,
     street_name,
-    station_geo,
+    ST_X(station_geo) as longitude,
+    ST_Y(station_geo) as latitude,
     bikes_available,
+    slots_available,
     total_capacity,
     round(safe_divide(bikes_available, total_capacity) * 100, 2) as occupancy_percent,
     case 
